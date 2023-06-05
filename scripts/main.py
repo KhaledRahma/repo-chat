58cc3f900a826d7dc1f877646c3c0e85aa959ef2
KhaledRahma <khaledrahma@hotmail.co.uk>
2023-06-05 16:02:51 +0100

    UI

--- scripts/main.py
+++ scripts/main.py
@@ -12,7 +12,7 @@ from langchain.schema import (
     SystemMessage
 )
 from langchain.chat_models import ChatOpenAI
-from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.base import BaseCallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 load_dotenv()
@@ -72,7 +72,7 @@ while True:
     Now answer the question using the code file(s) above.
     """
 
-    chat = ChatOpenAI(streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature = 0.5)
+    chat = ChatOpenAI(streaming=True, callback_manager=BaseCallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature = 0.5)
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
     chain = LLMChain(llm=chat, prompt=chat_prompt)
